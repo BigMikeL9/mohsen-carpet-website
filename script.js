a82b879e39e4841d6e5eeb6eab7368274c57f857
MikeL Kamel <mikel1832@yahoo.com>
2021-09-20 20:54:19 -0400

    add hamburger menu

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const responsiveMenu = document.querySelector('.responsive__menu');
+const navLinks = document.querySelector('.nav__links');
+
+// Responsive Menu
+responsiveMenu.addEventListener('click', function () {
+  responsiveMenu.classList.toggle('open');
+});
