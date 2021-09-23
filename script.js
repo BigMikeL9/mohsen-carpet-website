f7bf46bf346c34ad2299993683f65e37133bb1d7
MikeL Kamel <mikel1832@yahoo.com>
2021-09-22 21:07:57 -0400

    finish slide-in nav bar

--- script.js
+++ script.js
@@ -2,16 +2,37 @@
 
 const hamburgerMenu = document.querySelector('.nav__hamburger');
 const navDrawer = document.querySelector('.nav__drawer');
+const overlay = document.querySelector('.overlay');
+const navItems = document.querySelectorAll('.nav__item');
 
-// Responsive Menu
-hamburgerMenu.addEventListener('click', function () {
-  hamburgerMenu.classList.toggle('open');
-  navDrawer.classList.toggle('active');
-});
+// Menu Slider
+const menuSlideIn = function () {
+  // Toggle Nav
+  hamburgerMenu.addEventListener('click', function () {
+    hamburgerMenu.classList.toggle('open');
+    navDrawer.classList.toggle('active');
 
-document.addEventListener('click', function (event) {
-  hamburgerMenu.classList.toggle('open');
-  navDrawer.classList.toggle('active');
+    // Add Links Animations
+    navItems.forEach((item, index) => {
+      item.style.animation = `nav__item-animation 0.5s ease forwards ${
+        index / 5 + 0.2
+      }s`;
+    });
+  });
 
-  console.log(event.target);
-});
+  overlay.addEventListener('click', function () {
+    if (navDrawer.classList.contains('active')) {
+      navDrawer.classList.toggle('active');
+      hamburgerMenu.classList.toggle('open');
+    }
+
+    // Remove Links Animations
+    navItems.forEach(item => {
+      setTimeout(() => {
+        item.style.animation = '';
+      }, 500);
+    });
+  });
+};
+
+menuSlideIn();
