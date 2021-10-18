aacabccf07e65f9ea6a7a3ba6913518b6aad0bf4
MikeL Kamel <mikel1832@yahoo.com>
2021-10-17 20:20:04 -0400

    title color animation and Initialising Sass

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const navDrawer = document.querySelector('.nav__drawer');
 const overlay = document.querySelector('.overlay');
 const navItems = document.querySelectorAll('.nav__item');
 
-// Menu Slider
+//////////////////////////////////////////////
+// ***** Menu Slider
 const menuSlideIn = function () {
   // Toggle Nav
   hamburgerMenu.addEventListener('click', function () {
@@ -36,3 +37,36 @@ const menuSlideIn = function () {
 };
 
 menuSlideIn();
+
+//////////////////////////////////////////////
+// ***** Reveals Sections as we scroll down --> Intersection Observer API
+
+const allSections = document.querySelectorAll('.section');
+
+const sectionObsCallBack = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry.target); // the elements that we intersected
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+
+  // prevents observer from observing the current revealed section after it is revealed. [better for performance]
+  observer.unobserve(entry.target);
+};
+
+const sectionObsOptions = {
+  root: null,
+  threshold: 0.15, // greater than '0' because we dont want to show the section right as it enters the viewport, but a little bit later. Section will be revealed only when it is 15 percent visible.
+};
+
+const sectionObserver = new IntersectionObserver(
+  sectionObsCallBack,
+  sectionObsOptions
+);
+
+allSections.forEach(section => {
+  section.classList.add('section--hidden'); // change if necessary
+  sectionObserver.observe(section); // change if necessary
+});
+
+//////////////////////////////////////////////
